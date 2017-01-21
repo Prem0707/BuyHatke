bbe8afb1c238531ac751257df81da13ca1aadf84
Prem0707 <premprakash0796@gmail.com>
2017-01-21 22:42:28 +0530

    MyArticleData class added to hold all the links of photos

---
+++ app/src/main/java/com/example/android/buyhatke/MyArticleData.java
@@ -0,0 +1,42 @@
+package com.example.android.buyhatke;
+
+/**
+ * Created by Prem on 21-01-2017.
+ */
+public class MyArticleData {
+    static String[] articles = {
+
+            "https://api.learn2crack.com/android/images/donut.png",
+            "https://api.learn2crack.com/android/images/eclair.png",
+            "https://api.learn2crack.com/android/images/froyo.png",
+            "https://api.learn2crack.com/android/images/ginger.png",
+            "https://api.learn2crack.com/android/images/honey.png",
+            "https://api.learn2crack.com/android/images/icecream.png",
+            "https://api.learn2crack.com/android/images/jellybean.png",
+            "https://api.learn2crack.com/android/images/kitkat.png",
+            "https://api.learn2crack.com/android/images/lollipop.png",
+            "https://api.learn2crack.com/android/images/marshmallow.png",
+            "https://api.learn2crack.com/android/images/donut.png",
+            "https://api.learn2crack.com/android/images/eclair.png",
+            "https://api.learn2crack.com/android/images/froyo.png",
+            "https://api.learn2crack.com/android/images/ginger.png",
+            "https://api.learn2crack.com/android/images/honey.png",
+            "https://api.learn2crack.com/android/images/icecream.png",
+            "https://api.learn2crack.com/android/images/jellybean.png",
+            "https://api.learn2crack.com/android/images/kitkat.png",
+            "https://api.learn2crack.com/android/images/lollipop.png",
+            "https://api.learn2crack.com/android/images/marshmallow.png",
+            "https://api.learn2crack.com/android/images/donut.png",
+            "https://api.learn2crack.com/android/images/eclair.png",
+            "https://api.learn2crack.com/android/images/froyo.png",
+            "https://api.learn2crack.com/android/images/ginger.png",
+            "https://api.learn2crack.com/android/images/honey.png",
+            "https://api.learn2crack.com/android/images/icecream.png",
+            "https://api.learn2crack.com/android/images/jellybean.png",
+            "https://api.learn2crack.com/android/images/kitkat.png",
+            "https://api.learn2crack.com/android/images/lollipop.png",
+            "https://api.learn2crack.com/android/images/marshmallow.png",
+            "https://api.learn2crack.com/android/images/donut.png"
+    };
+
+}
