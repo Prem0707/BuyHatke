0e24cf4382c009786ab8d8396792a856266d3d9e
Prem0707 <premprakash0796@gmail.com>
2017-01-21 22:55:55 +0530

    AnArticle class returns links of images

---
+++ app/src/main/java/com/example/android/buyhatke/AnArticle.java
@@ -0,0 +1,13 @@
+package com.example.android.buyhatke;
+
+/**
+ * Created by Prem on 21-01-2017.
+ */
+public class AnArticle {
+
+    String imageUrl;
+    public AnArticle(String imageUrl) {
+
+        this.imageUrl = imageUrl;
+    }
+}
